a573472f280ef044723b366601fd6aca62a8ba45
Caven <cavenasdev@gmail.com>
2024-08-02 01:45:25 +0800

    feat: add NotFound router

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import ProfileView from '@/components/view/ProfileView/Index.vue'
 import ProfileFormView from '@/components/view/ProfileFormView/Index.vue'
+import NotFound from '@/components/view/NotFound.vue'
 
 const routes = [
-  { path: '/:id?', component: ProfileView },
-  { path: '/profile-form/:id?', component: ProfileFormView },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+  { path: '/:id?', component: ProfileView, alias: '/profile/:id?' },
+  { path: '/profile/edit/:id?', component: ProfileFormView },
+  { path: '/profile/create', component: ProfileFormView },
 ]
 
 export const router = createRouter({
