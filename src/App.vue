1c6084d89c47abf10203937c93b86e58ac371aad
Caven <cavenasdev@gmail.com>
2024-08-01 10:11:46 +0800

    feat: add Cover

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import Cover from './components/profile/Cover.vue'
 import Layout from './components/profile/Layout.vue'
 </script>
 
 <template>
   <main class="w-full h-full bg-zinc-100 overflow-auto">
-    <Layout />
+    <Layout>
+      <Cover />
+    </Layout>
   </main>
 </template>
